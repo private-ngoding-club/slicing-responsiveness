82c76a23f130dc20701b0f1c64097663f2bc1987
westtle <cuberkuber@gmail.com>
2023-03-16 06:55:51 +0700

    refactor: change how the sidebar button looked

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,14 @@
 const sidebar = document.querySelector("#sidebar");
-const sidebarButton = document.querySelector("#sidebar__button");
+const sidebarButton = document.querySelectorAll(".sidebar__button");
 
-sidebarButton.addEventListener("click", () => {
-	let opened = sidebarButton.getAttribute("data-sidebar-expanded");
+sidebarButton.forEach((button) => {
+	button.addEventListener("click", () => {
+		let opened = sidebar.getAttribute("aria-expanded");
 
-	if (opened == "false") {
-		sidebarButton.dataset.sidebarExpanded = "true";
-		// sidebar.dataset.sidebarExpanded = "true";
-		sidebar.setAttribute("aria-expanded", true);
-	} else {
-		sidebarButton.dataset.sidebarExpanded = "false";
-		// sidebar.dataset.sidebarExpanded = "false";
-		sidebar.setAttribute("aria-expanded", false);
-	}
+		if (opened == "false") {
+			sidebar.setAttribute("aria-expanded", true);
+		} else {
+			sidebar.setAttribute("aria-expanded", false);
+		}
+	});
 });
